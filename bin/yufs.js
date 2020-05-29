633910f5c8d151dca28dafc4aec291ed70087415
nalanrola <nalanrola@gmail.com>
2020-05-30 01:46:14 +0800

    优化

--- bin/yufs.js
+++ bin/yufs.js
@@ -8,11 +8,19 @@ program.version('v'+require("../package.json").version, '-v, --version')
 .description("This is a npm file system  command line tool!");
 
 program.command('copy <src> <dst>')
-.alias('-c')
+.alias('c')
 .description('copy file or directory from src to dst')
 .action(function(src, dst) {
     fo.copy(src, dst);
     console.log('copy successfully!');
+})
+
+program.command('move <src> <dst>')
+.alias('m')
+.description('move file or directory from src to dst')
+.action(function(src, dst) {
+   if(fo.move(src, dst))
+        console.log('move successfully!');
 })
 
 program.parse(process.argv);
